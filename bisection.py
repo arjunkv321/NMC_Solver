f24390d7601b64f53e1cac8373aad5867f348088
Arjun <68591891+arjunkv321@users.noreply.github.com>
2020-10-02 12:42:12 +0530

    Update bisection.py

--- bisection.py
+++ bisection.py
@@ -1,18 +1,28 @@
 import math
-a=1
-b=2
-r=6
-z=11
+import sympy
+from sympy import *
+
+x=Symbol('x')
+e=sympify(input("Enter function in x: "))
+expression=e.subs({'x':x})
+f=lambdify(Symbol('x'),expression,'numpy')
+
+a=float(input("Enter initial value of range : "))         #input range seperated by ,
+b=float(input("Enter final value of range : "))
+num_iter=int(input("Enter number of iteration: "))         #enter number of iteration
+round_of=int(input("Enter round of number :"))          #enter the number of iteration
 c=0
 
-def eq(x):
-    fx=x**3-x-1
-    return(fx)
-for i in range(z):
+for i in range(num_iter):
     q=c
-    c=round((a+b)/2,r)
-    print("a={:<12} b={:<12} c={:<12} f(c)={:<20} epsa={:<12}".format(round(a,r),round(b,r),round(c,r),round((eq(c)),r),abs(round((q-c)/c*100,r))))
-    if (eq(c))<0:
+    c=(a+b)/2
+    print("Sl.No {:<10} a={:<12} b={:<12} c={:<12} f(c)={:<20} epsa={:<12}".format(i,round(a,round_of),round(b,round_of),round(c,round_of),round((f(c)),round_of),abs(round((q-c)/c*100,round_of))))
+    if (f(c))<0:
         a=c
     else:
         b=c
+
+while True:
+    y=input("Enter 'q' to exit : ")
+    if y=="q" or y=="Q":
+        break
